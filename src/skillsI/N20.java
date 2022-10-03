edf3783916c642cef24056e66f3097cad4bc66fa
beekeepeer <dmitrygoryuk@gmail.com>
2022-10-03 16:32:51 +0300

    Added one exercise

--- src/skillsI/N20.java
+++ src/skillsI/N20.java
@@ -2,18 +2,30 @@ package skillsI;
 
 public class N20 {
     public boolean isValid(String s) {
-        s = s.replaceAll("\\(\\)", "").replaceAll("\\[\\]", "").replaceAll("\\{\\}", "");
-        if(s.length() > 0){
-            return false;
+        int flag = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                flag += 1;
+            } else if (s.charAt(i) == ')') {
+                flag -= 1;
+            } else if (s.charAt(i) == '[') {
+                flag += 1;
+            } else if (s.charAt(i) == ']') {
+                flag -= 1;
+            } else if (s.charAt(i) == '{') {
+                flag += 1;
+            } else if (s.charAt(i) == '}') {
+                flag -= 1;
+            }
         }
-        return true;
-        }
-    }
-
-
-class Test20 {
-    public static void main(String[] args) {
-        N20 n20 = new N20();
-        System.out.println(n20.isValid("()[]{}"));
+        return flag == 0;
     }
 }
+
+
+    class Test20 {
+        public static void main(String[] args) {
+            N20 n20 = new N20();
+            System.out.println(n20.isValid("()[])({}"));
+        }
+    }
